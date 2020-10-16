36fb2a062f123b625739af25d7c42326490ca730
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2020-10-16 13:20:10 +0200

    Add HITO logo.

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ CSRF_ENABLED = True
 # APP_NAME = "My App Name"
 
 # Uncomment to setup Setup an App icon
-# APP_ICON = "static/img/logo.jpg"
+APP_ICON = "/static/img/hito.svg"
 
 # ----------------------------------------------------
 # AUTHENTICATION CONFIG
