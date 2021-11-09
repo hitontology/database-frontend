7acb7556a874f018c65c87405d50093df81e9cba
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2021-11-09 10:49:22 +0100

    Always try to create the admin user in case it fails but still creates the file.

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-if [ ! -f "admin" ]
-then
+# The conditional sometimes fails but still creates the admin file.
+#if [ ! -f "admin" ]
+#then
   flask fab create-admin --username admin --firstname admin --lastname admin --email nomail --password $HITO_DATABASE_PASSWORD
-  touch admin
-fi
+#  touch admin
+#fi
 flask run --host=0.0.0.0
