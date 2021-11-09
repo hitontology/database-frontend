ad8444578573f1281c846b67718379c5ee7e7115
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2021-11-09 14:33:15 +0100

    Support the reverse proxy path.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 import logging
-
+from warnings import warn
+import os
 from flask import Flask
 from flask_appbuilder import AppBuilder, SQLA
+from flask_reverse_proxy_fix.middleware import ReverseProxyPrefixFix
 
 """
  Logging configuration
@@ -11,6 +13,16 @@ logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 logging.getLogger().setLevel(logging.WARN)
 
 app = Flask(__name__)
+#######################################################
+try:
+    REVERSE_PROXY_PATH = os.environ['HITO_DATABASE_FRONTEND_REVERSE_PROXY_PATH']
+    app.config['REVERSE_PROXY_PATH'] = REVERSE_PROXY_PATH
+    ReverseProxyPrefixFix(app)
+    print('HITO_DATABASE_FRONTEND_REVERSE_PROXY_PATH='+REVERSE_PROXY_PATH)
+except:
+    warn('HITO_DATABASE_FRONTEND_REVERSE_PROXY_PATH environment variable not set. This installation may not work when served under a folder.')
+
+#######################################################
 app.config.from_object("config")
 db = SQLA(app)
 appbuilder = AppBuilder(app, db.session, base_template='base.html')
