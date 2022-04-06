8bd9bb8a4f130c31f250d60c2667f7edc3a3e424
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-04-06 12:13:25 +0200

    Retry creating database frontend admin multiple times in case the database isn't up yet.

--- start.sh
+++ start.sh
@@ -1,8 +1,20 @@
-#!/bin/sh
-# The conditional sometimes fails but still creates the admin file.
-#if [ ! -f "admin" ]
-#then
+#!/bin/bash
+for t in 1 2 4 8 16 32 0
+do
   flask fab create-admin --username admin --firstname admin --lastname admin --email nomail --password $HITO_DATABASE_PASSWORD
-#  touch admin
-#fi
-flask run --host=0.0.0.0
+  if [ "$?" == "0" ]
+  then
+    flask run --host=0.0.0.0
+    break
+  else
+    >&2 echo -n "Failed to create admin user on the database. "
+	if [ "$t" == "0" ]
+	then
+      >&2 echo "Aborting."
+	  exit 1
+    else
+	  >&2 echo "Retrying in ${t} seconds."
+      sleep $t
+	fi
+  fi
+done
